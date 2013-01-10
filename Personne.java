1d3f67b12b7e34e9ac1d7e14696275c550728f09
Marie <girardin.lucas@gmail.com>
2013-01-10 16:17:21 +0100

    ajout de l'attribut genre

--- Personne.java
+++ Personne.java
@@ -7,6 +7,7 @@ public class Personne {
 	private String prenom;
 	private String adresse;
 	private String telephone;
+	private String genre;
 	private ArrayList <String>  listeAttribut;
 	
 	// Get & Set
